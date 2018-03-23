e62755ca258507bb3ff5448a34bc14b7800b6526
yemiwebby <yemiwebby@gmail.com>
2018-03-23 05:03:12 +0100

    Added another cryptocurrency

--- src/components/Tickers.js
+++ src/components/Tickers.js
@@ -36,10 +36,36 @@ class Tickers extends Component {
                     percent_change_1h: "0",
                     percent_change_24h: "0",
                     percent_change_7d: "0",
+                },
+                {
+                    id: "ripple",
+                    name: "Ripple",
+                    symbol: "XRP",
+                    price_usd: "1",
+                    percent_change_1h: "0",
+                    percent_change_24h: "0",
+                    percent_change_7d: "0",
                 }
             ]
         };
     }
+
+    componentDidMount() {
+        this.fetchCryptocurrencyData();
+        this.interval = setInterval(() => {
+            this.fetchCryptocurrencyData(), 10 * 1000
+        });
+    }
+
+    fetchCryptocurrencyData() {
+        axios.get("https://api.coinmarketcap.com/v1/ticker/?limit=10")
+            .then(response => {
+                var wanted = ["bitcoin", "ethereum", "litecoin", "ripple"];
+                var result = response.data.filter(currency => wanted.includes(currency.id));
+                this.setState({ data: result});
+            })
+            .catch(err => console.log(err));
+        }
 
     render() {
         var tickers = this.state.data.map((currency) =>
